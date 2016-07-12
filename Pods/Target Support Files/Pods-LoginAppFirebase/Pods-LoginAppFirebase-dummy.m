07c84d98c16fc483176ef1420d2966fdf7f07680
Lisandro Falconi <lisandrofalconi@gmail.com>
2016-07-11 21:55:56 -0300

    Inited Podfil and added Firebase/Auth pods

---
+++ Pods/Target Support Files/Pods-LoginAppFirebase/Pods-LoginAppFirebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoginAppFirebase : NSObject
+@end
+@implementation PodsDummy_Pods_LoginAppFirebase
+@end
---
+++ Pods/Target Support Files/Pods-LoginAppFirebase/Pods-LoginAppFirebase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LoginAppFirebaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LoginAppFirebaseVersionString[];
+
